0f50f3b9a6dba3141f0246257c33d4a75be88117
imPDA <dimaxa7@mail.ru>
2025-03-01 19:14:31 +0300

    fix for logger

--- logger.lua
+++ logger.lua
@@ -1,17 +1,14 @@
 --#region LOGGER
 function IMP_STATS_Logger(name)
-    local logger
+    if not LibDebugLogger then return function(...) end end
 
-    if LibDebugLogger then
-        logger = LibDebugLogger:Create(name or 'IMP_STATS')
-        logger:SetMinLevelOverride(LibDebugLogger.LOG_LEVEL_DEBUG)
-    end
+    local logger = LibDebugLogger:Create(name or 'IMP_STATS')
+    logger:SetMinLevelOverride(LibDebugLogger.LOG_LEVEL_DEBUG)
 
     local level = LibDebugLogger.LOG_LEVEL_DEBUG
-    local function inner (...)
-        if logger then
-            logger:Log(level, ...)
-        end
+
+    local function inner(...)
+        logger:Log(level, ...)
     end
 
     return inner
